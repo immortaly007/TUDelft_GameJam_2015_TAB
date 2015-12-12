ca4d02677210907819a98c33a4817b1fb05c06aa
Bas Dado <immortaly007@gmail.com>
2015-12-12 16:20:45 +0100

    Unity scene

---
+++ Matrix/Assets/UIProjector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIProjector : MonoBehaviour {
+
+    public Camera camera;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
