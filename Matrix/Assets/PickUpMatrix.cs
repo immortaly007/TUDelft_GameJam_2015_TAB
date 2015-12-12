f81b2141dacd87944c861ee55788bd041986d525
TommieV <TommieV@Gmail.com>
2015-12-12 21:26:32 +0100

    added matrix collectibles

---
+++ Matrix/Assets/PickUpMatrix.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpMatrix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public bool rotation;
+	public bool translation;
+	public bool scale;
+	public float angle;
+	public float dx;
+	public float dy;
+	public float sx;
+	public float sy;
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		MatrixModifier myMatrix = null;
+		if (rotation) {
+			myMatrix = new RotationMatrixModifier (angle);
+		}
+		if (translation) {
+			myMatrix = new TranslationMatrixModifier (new Vector2(dx,dy));
+		}
+		if (scale) {
+			myMatrix = new ScalingMatrixModifier(new Vector2(sx,sy));
+		}
+		InventoryManager.instance.AddMatrixModifier (myMatrix);
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
