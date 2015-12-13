332f63efc8d5d6be58ce02ceb0c729a17a5a3f40
TommieV <TommieV@Gmail.com>
2015-12-13 10:41:43 +0100

    Added canvas manager to remove annoying canvas and slightly repositioned 1 building

---
+++ Matrix/Assets/canvasManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class canvasManager : MonoBehaviour {
+
+
+	public GameObject myCanvas;
+	// Use this for initialization
+	void Start () {
+		myCanvas.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
