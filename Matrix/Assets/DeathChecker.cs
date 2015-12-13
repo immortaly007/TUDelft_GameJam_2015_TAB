28bce6a22c4a9e3d6a083c03f2d7c3894076d382
Bas Dado <immortaly007@gmail.com>
2015-12-13 03:35:10 +0100

    Merging

--- Matrix/Assets/DeathChecker.cs
+++ Matrix/Assets/DeathChecker.cs
@@ -6,7 +6,7 @@ public class DeathChecker : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the player died
-        if (other.gameObject.tag == "DeathZone")
+        if (other.gameObject.tag == "DeathCheck")
             CheckpointManager.Instance.RestoreLastCheckpoint(transform);
 
         if (other.gameObject.tag == "CheckPoint")
