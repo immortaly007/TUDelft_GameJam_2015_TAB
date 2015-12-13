1cd0e8d167374c5083ee9d85bfbde16ba6ca27a1
Bas Dado <immortaly007@gmail.com>
2015-12-13 02:22:41 +0100

    Basic checkpoint funcitonality

---
+++ Matrix/Assets/DeathChecker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathChecker : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the player died
+        if (other.gameObject.tag == "DeathZone")
+            CheckpointManager.Instance.RestoreLastCheckpoint(transform);
+
+        if (other.gameObject.tag == "CheckPoint")
+            CheckpointManager.Instance.SetCheckpoint(transform);
+
+    }
+}
