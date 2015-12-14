a82ebd13723b1249f9a4b47cccb97ab106122dff
Bas Dado <immortaly007@gmail.com>
2015-12-14 02:16:35 +0100

    Matrix rain fix

--- Matrix/Assets/Scripts/MatrixRainStatic.cs
+++ Matrix/Assets/Scripts/MatrixRainStatic.cs
@@ -12,16 +12,16 @@ public class MatrixRainStatic : MonoBehaviour {
     public float spawnSpeed; // spawnspeed in symbols per second
 
     private float timePast;
-    private Collider2D collider;
+    private Collider2D boundingCollider;
 
     void Start()
     {
-        collider = GetComponent<Collider2D>();
+        boundingCollider = GetComponent<Collider2D>();
     }
 
 	// Update is called once per frame
 	void Update () {
-        float expectedSymbols = Time.deltaTime * spawnSpeed * collider.bounds.size.x * collider.bounds.size.y;
+        float expectedSymbols = Time.deltaTime * spawnSpeed * boundingCollider.bounds.size.x * boundingCollider.bounds.size.y;
         if (expectedSymbols > 1f || Random.value < expectedSymbols)
         {
             int symbolCount = (int)expectedSymbols;
@@ -30,9 +30,9 @@ public class MatrixRainStatic : MonoBehaviour {
             for (int i = 0; i < symbolCount; i++)
             {
                 // Pick a random position within the collider space
-                Vector3 randOffset = new Vector3(Random.Range(0f, collider.bounds.size.x), Random.Range(0f, collider.bounds.size.y), 0f);
-                Vector3 possiblePosition = collider.bounds.min + randOffset;
-                if (collider.OverlapPoint(possiblePosition))
+                Vector3 randOffset = new Vector3(Random.Range(0f, boundingCollider.bounds.size.x), Random.Range(0f, boundingCollider.bounds.size.y), 0f);
+                Vector3 possiblePosition = boundingCollider.bounds.min + randOffset;
+                if (boundingCollider.OverlapPoint(possiblePosition))
                 {
                     var symbolGO = MatrixSymbolPool.instance.Get(possiblePosition);
                     var textMesh = symbolGO.GetComponent<TextMesh>();
